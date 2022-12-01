b6db70858b2663adb2f2a9eaa4ee471d99a60ec0
DaveC <dave.campbell@innovateauto.com>
2022-12-01 12:23:44 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='capitalize',
+      version='1.0',
+      # list folders, not files
+      packages=['capitalize',
+                'capitalize.test'],
+      scripts=['capitalize/bin/cap_script.py'],
+      package_data={'capitalize': ['data/cap_data.txt']},
+      )
+
